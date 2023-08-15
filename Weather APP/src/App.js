54bad26310209ea3a2390f78d9c34278334bb6e5
Pramitha Jayasooriya <lpramithamj@gmail.com>
2023-08-15 20:15:20 +0530

    add test

--- Weather APP/src/App.js
+++ Weather APP/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import WeatherService from "./components/WeatherService";
+import Test from "./components/Test";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<WeatherService />}></Route>
+          <Route path="/" element={<Test />}></Route>
         </Routes>
       </Router>
     </div>
